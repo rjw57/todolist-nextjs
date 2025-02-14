e0bc6e459dd7a8e9c26230b37f75bf52fc4aca5b
Rich Wareham <rich@richwareham.com>
2025-02-14 13:38:42 +0000

    feat: add eslint config and pre-commit config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import stylistic from "@stylistic/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: { stylistic },
+    rules: {
+      "stylistic/max-len": [
+        "error",
+        {
+          code: 100,
+          tabWidth: 2,
+          // To allow for long JSX properties.
+          ignoreStrings: true,
+        },
+      ],
+      indent: ["error", 2],
+    },
+  },
 ];
 
 export default eslintConfig;
